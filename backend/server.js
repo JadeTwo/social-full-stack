4c9bf26ed9360a8bbb83438bcfad0d05a0d9b433
jadedrb <jadecdrb@gmail.com>
2026-03-27 08:48:35 -0400

    removed origin for now

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use(cors({ origin: [process.env.CLIENT_ORIGIN, 'http://localhost:5173'] }))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/posts', postRoutes)
